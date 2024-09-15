06a8ca04d66a6a152eae021fc5c9e30fd87dc2cc
Maurya Rajeev <104668455+mauryarajeev@users.noreply.github.com>
2024-09-15 20:32:02 +0530

    Update Dashboard.tsx

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -4,22 +4,23 @@ import ChartList from "../components/ChartList";
 import DataCardList from "../components/DataCardList";
 import CardList from "../components/CardList";
 import Footer from "../components/Footer";
+
 const Dashboard: React.FC = () => {
   return (
-    <div className="flex flex-col items-center gap-8 p-6">
-      <div className="w-full p-4 border border-gray-200 rounded-lg shadow-md">
+    <div className="flex flex-col items-center gap-8 p-4 md:p-6">
+      <div className="w-full md:p-4 md:border md:border-gray-200 md:rounded-lg md:shadow-md">
         <SalesByCountry />
       </div>
-      <div className="w-full  p-4 border border-gray-200 rounded-lg shadow-md">
+      <div className="w-full md:p-4 md:border md:border-gray-200 md:rounded-lg md:shadow-md">
         <ChartList />
       </div>
-      <div className="w-full  p-4 border border-gray-200 rounded-lg shadow-md">
+      <div className="w-full md:p-4 md:border md:border-gray-200 md:rounded-lg md:shadow-md">
         <DataCardList />
       </div>
-      <div className="w-full  p-4 border border-gray-200 rounded-lg shadow-md">
+      <div className="w-full md:p-4 md:border md:border-gray-200 md:rounded-lg md:shadow-md">
         <CardList />
       </div>
-       <div className="w-full gap-0 p-0 border-gray-200 rounded-lg shadow-md">
+      <div className="w-full md:p-4 md:border md:border-gray-200 md:rounded-lg md:shadow-md">
         <Footer />
       </div>
     </div>
